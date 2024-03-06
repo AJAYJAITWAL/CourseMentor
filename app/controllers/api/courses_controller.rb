5531474fc46ccfaac1ffb8d20e526f9ebab40708
AJAYJAITWAL <ajayjaitwal3434@gmail.com>
2024-03-06 18:19:33 +0530

    Add controller for that

---
+++ app/controllers/api/courses_controller.rb
@@ -0,0 +1,22 @@
+class Api::CoursesController < ApplicationController
+  def create
+    course = Course.new(course_params)
+
+    if course.save
+      render json: course, status: :created
+    else
+      render json: { errors: course.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def index
+    courses = Course.all
+    render json: courses, include: :tutors
+  end
+
+  private
+
+  def course_params
+    params.require(:course).permit(:name, tutors_attributes: [:name])
+  end
+end
